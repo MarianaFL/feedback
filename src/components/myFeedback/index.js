dfb184ffaf12a1367cead54c0da2cc77fc5f15dc
Belmino <belminoj@gmail.com>
2017-03-28 09:50:39 -0300

    Adicionando arquivo Card.js, trocando new Date() por datas formatadas e puxando os dados do State/Firebase

--- src/components/myFeedback/index.js
+++ src/components/myFeedback/index.js
@@ -1,46 +1,39 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux';
 import { feedbackDispatchers } from 'api/actions';
-
-
+import Card from 'components/Card';
 
 class MyFeedback extends React.Component {
   render() {
   	const { myfeedback } = this.props
-	const feedbackContainer = myfeedback.getIn(['feedbacks']).map((object) => (
+    const listFeedbacks = myfeedback.getIn(['feedbacks']).toJS();
 
-		<div>
-			{object.get('description')}
-		</div> 
-		//TODO
+/*
+    //Não funcionou
+    const containerFeedback = listFeedbacks.map((object) => (
+      <Card description={object.get('description')} surveyDate={object.get('surveyDate')} />
   	))
-
+*/
+    let containerFeedback = []
+    for (let object in listFeedbacks){
+      containerFeedback.push(<Card description={listFeedbacks[object]['description']} surveyDate={listFeedbacks[object]['surveyDate']} />);
+      /*
+      //Funciona
+      containerFeedback.push(<div className="mdc-card">
+        <section className="mdc-card__primary">
+          <h1 className="mdc-card__title mdc-card__title--large">{listFeedbacks[object]['description']}</h1>
+          <h2 className="mdc-card__subtitle" >{listFeedbacks[object]['surveyDate']}</h2>
+        </section>
+      </div>)
+      */
+    }
      return (
-      <div>
-      	<h1 >
-  			Feedback
-      	</h1>
-
-      		
-		  <div className="mdc-card">
-		    <section className="mdc-card__primary">
-			      <h1 className="mdc-card__title mdc-card__title--large">Preciso que vocês me avaliem.</h1>
-			      <h2 className="mdc-card__subtitle" >25/03/2017</h2>
-		    </section>
-		  </div>
-		  <div className="mdc-card">
-		    <section className="mdc-card__primary">
-			      <h1 className="mdc-card__title mdc-card__title--large" >Preciso que vocês me avaliem.</h1>
-			      <h2 className="mdc-card__subtitle" >25/03/2017</h2>
-		    </section>
-		  </div>
-		  <div className="mdc-card">
-		    <section className="mdc-card__primary">
-			      <h1 className="mdc-card__title mdc-card__title--large" >Preciso que vocês me avaliem.</h1>
-			      <h2 className="mdc-card__subtitle">25/03/2017</h2>
-		    </section>
-		  </div>
-      </div>
+       <div>
+         <h1 >
+           Feedback
+         </h1>
+         {containerFeedback}
+       </div>
     );
   }
 }
